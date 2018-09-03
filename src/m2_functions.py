3b5ee3e31324a3ec1b5df7a9b14b46c29c338919
Sutidamus <koriasutibun@gmail.com>
2018-09-03 16:20:45 -0400

    DONE

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -8,7 +8,7 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -26,11 +26,16 @@ def main():
     TESTS the functions that you will write below.
     You write the tests per the _TODO_s below.
     """
-
+    ans = hypo(3, 4)
+    print(ans)
+    window = rg.TurtleWindow()
+    setup('red', 20)
+    setup('blue', 10)
+    window.close_on_mouse_click()
 
 ###############################################################################
 #
-# TODO: 3a.  Define a function immediately blow this _TODO_.
+# DONE  Define a function immediately blow this _TODO_.
 #   It takes two arguments that denote, for a right triangle,
 #   the lengths of the two sides adjacent to its right angle,
 #   and it returns the length of the hypotenuse of that triangle.
@@ -38,15 +43,20 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 3b.  In main, CALL your function and print the returned value,
+# DONE  In main, CALL your function and print the returned value,
 #   to test whether you defined the function correctly.
 #
 ###############################################################################
 
+def hypo(side1, side2):
+    s_1 = side1
+    s_2 = side2
+    hyp = (s_1**2 + s_2**2)**(1/2)
+    return hyp
 
 ###############################################################################
 #
-# TODO: 4a.  Define a function immediately below this _TODO_.
+# DONE: 4a.  Define a function immediately below this _TODO_.
 #   It takes two arguments:
 #     -- a string that represents a color (e.g. 'red')
 #     -- a positive integer that represents the thickness of a Pen.
@@ -68,10 +78,27 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 4b.  In main, CALL your function at least TWICE (with different values
+# DONE: 4b.  In main, CALL your function at least TWICE (with different values
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
+import rosegraphics as rg
+
+def setup(color, thicc):
+    c = color
+    t = thicc
+
+    bob = rg.SimpleTurtle('turtle')
+    john = rg.SimpleTurtle('turtle')
+
+    bob.pen = rg.Pen('green', 5)
+    john.pen = rg.Pen(c, t)
+
+    john.forward(100)
+    bob.backward(100)
+
+
+
 
 
 ###############################################################################
